4fa47157d124a0a9dcb8de3d36422af828330582
Paulo Tome <paulo.tome@siscog.pt>
2014-03-26 12:07:43 +0000

    Added encoding section in file init.el.

--- init.el
+++ init.el
@@ -86,11 +86,9 @@
 (modify-syntax-entry ?.  "w   " lisp-mode-syntax-table)
 
 
-;;Desactivar tooltips no emacs 23 é só avaliar a expressão:
+;; Deactivate tooltips in emacs:
 
 (tooltip-mode 0)
-
-;;Quem não quiser a toolbar, que só rouba espaço, também pode utilizar:
 
 ;;(tool-bar-mode 0)
 (tool-bar-mode -1)
@@ -285,3 +283,69 @@ Finaly, blinks at the end of the marked region."
           (rename-buffer new-name)
           (set-visited-file-name new-name)
           (set-buffer-modified-p nil))))))
+
+
+;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; ENCODING ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
+;; C-h C RET
+;; M-x describe-current-coding-system
+
+(add-to-list 'file-coding-system-alist '("\\.tex" . utf-8-unix) )
+(add-to-list 'file-coding-system-alist '("\\.txt" . utf-8-unix) )
+(add-to-list 'file-coding-system-alist '("\\.el" . utf-8-unix) )
+(add-to-list 'file-coding-system-alist '("\\.scratch" . utf-8-unix) )
+(add-to-list 'file-coding-system-alist '("user_prefs" . utf-8-unix) )
+
+(add-to-list 'process-coding-system-alist '("\\.txt" . utf-8-unix) )
+
+(add-to-list 'network-coding-system-alist '("\\.txt" . utf-8-unix) )
+
+(prefer-coding-system 'utf-8-unix)
+(set-default-coding-systems 'utf-8-unix)
+(set-terminal-coding-system 'utf-8-unix)
+(set-keyboard-coding-system 'utf-8-unix)
+(set-selection-coding-system 'utf-8-unix)
+(setq-default buffer-file-coding-system 'utf-8-unix)
+
+;; Treat clipboard input as UTF-8 string first; compound text next, etc.
+(setq x-select-request-type '(UTF8_STRING COMPOUND_TEXT TEXT STRING))
+
+;; mnemonic for utf-8 is "U", which is defined in the mule.el
+(setq eol-mnemonic-dos ":CRLF")
+(setq eol-mnemonic-mac ":CR")
+(setq eol-mnemonic-undecided ":?")
+(setq eol-mnemonic-unix ":LF")
+
+(defalias 'read-buffer-file-coding-system 'lawlist-read-buffer-file-coding-system)
+(defun lawlist-read-buffer-file-coding-system ()
+  (let* ((bcss (find-coding-systems-region (point-min) (point-max)))
+         (css-table
+          (unless (equal bcss '(undecided))
+            (append '("dos" "unix" "mac")
+                    (delq nil (mapcar (lambda (cs)
+                                        (if (memq (coding-system-base cs) bcss)
+                                            (symbol-name cs)))
+                                      coding-system-list)))))
+         (combined-table
+          (if css-table
+              (completion-table-in-turn css-table coding-system-alist)
+            coding-system-alist))
+         (auto-cs
+          (unless find-file-literally
+            (save-excursion
+              (save-restriction
+                (widen)
+                (goto-char (point-min))
+                (funcall set-auto-coding-function
+                         (or buffer-file-name "") (buffer-size))))))
+         (preferred 'utf-8-unix)
+         (default 'utf-8-unix)
+         (completion-ignore-case t)
+         (completion-pcm--delim-wild-regex ; Let "u8" complete to "utf-8".
+          (concat completion-pcm--delim-wild-regex
+                  "\\|\\([[:alpha:]]\\)[[:digit:]]"))
+         (cs (completing-read
+              (format "Coding system for saving file (default %s): " default)
+              combined-table
+              nil t nil 'coding-system-history
+              (if default (symbol-name default)))))
+    (unless (zerop (length cs)) (intern cs))))
