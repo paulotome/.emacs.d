950879e5469b8263a76723e0178d582be11b2adc
Paulo Jorge Tomé <paulo.jorge.tome@gmail.com>
2014-04-26 01:54:26 +0100

    Modified file init.el.

--- init.el
+++ init.el
@@ -360,10 +360,3 @@ Finaly, blinks at the end of the marked region."
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;; ENCODING ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
 (setq org-list-allow-alphabetical t)
 
-
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(default ((t (:family "DejaVu Sans Mono" :foundry "unknown" :slant normal :weight normal :height 98 :width normal)))))
