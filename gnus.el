bf2ca24116ee0b441ea3c514624549e133c3c02b
Paulo Tome <paulo.tome@siscog.pt>
2014-05-13 18:26:55 +0100

    Updated file gnus.el.

--- gnus.el
+++ gnus.el
@@ -9,25 +9,25 @@
 ;; ******************************************************
 ;; *                      GMAIL		         	*
 ;; ******************************************************
-(setq gnus-select-method
-      '(nnimap "gmail"
-	(nnimap-address "imap.gmail.com")
-	(nnimap-server-port 993)
-	(nnimap-stream ssl)
-	(nnimap-authinfo-file "~/.authinfo")
-	))
+(setq gnus-select-method '(nnimap "gmail"
+			   (nnimap-address "imap.gmail.com")
+			   (nnimap-server-port 993)
+			   (nnimap-stream ssl)
+			   (nnimap-authinfo-file "~/.authinfo")
+			   ))
 
 
-(setq message-send-mail-function 'smtpmail-send-it
-      smtpmail-starttls-credentials '(("smtp.gmail.com" 587 nil nil))
-      smtpmail-auth-credentials '(("smtp.gmail.com" 587
-				   "paulo.jorge.tome@gmail.com" nil))
-      smtpmail-default-smtp-server "smtp.gmail.com"
-      smtpmail-smtp-server "smtp.gmail.com"
-      smtpmail-smtp-service 587
-      smtpmail-debug-info t ; optional, but handy in case something goes wrong
-      ;; Make Gnus NOT ignore [Gmail] mailboxes
-      gnus-ignored-newsgroups "^to\\.\\|^[0-9. ]+\\( \\|$\\)\\|^[\"]\"[#'()]")
+(setq
+ smtpmail-starttls-credentials '(("smtp.gmail.com" 587 nil nil))
+ smtpmail-auth-credentials '(("smtp.gmail.com" 587
+			      "paulo.jorge.tome@gmail.com" nil))
+ smtpmail-default-smtp-server "smtp.gmail.com"
+ smtpmail-smtp-server "smtp.gmail.com"
+ smtpmail-smtp-service 587
+ smtpmail-debug-info t ; optional, but handy in case something goes wrong
+ ;; Make Gnus NOT ignore [Gmail] mailboxes
+ gnus-ignored-newsgroups "^to\\.\\|^[0-9. ]+\\( \\|$\\)\\|^[\"]\"[#'()]"
+ )
 
 
 (setq gnus-parameters '(("nnimap\\+gmail:INBOX" (expiry-wait . 2))))
@@ -37,9 +37,9 @@
 ;; ******************************************************
 
 
-(setq gnus-secondary-select-methods nil)
+(setq gnus-secondary-select-methods '((nnml "")))
+
 (setq gnus-select-group-hook nil)
-(setq gnus-auto-select-next t)
 (setq gnus-summary-check-current t)
 (setq gnus-auto-center-summary nil)
 (setq gnus-thread-indent-level 1)
@@ -47,19 +47,7 @@
 (setq gnus-add-to-list t)
 (setq gnus-summary-display-while-building 10)
 
-;;; Fetch no older articles, but thread everything we got.
-;; (setq gnus-fetch-old-headers 'nil)
-(setq gnus-fetch-old-headers 'some)
 
-
-;;;; trier :
-;; par date (plus recent en premier) puis par sujets
-;;;(setq gnus-thread-sort-functions
-;;;      '(
-;;;	gnus-thread-sort-by-total-score
-;;;        gnus-thread-sort-by-subject
-;;;        (not gnus-thread-sort-by-date)
-;;;        ))
 
 ;;; Remove mail we have already fetched. Only reason to have this set
 ;;; to anything else than t is that you think Gnus will lose your
@@ -67,8 +55,7 @@
 ;; (setq mail-source-delete-incoming t)
 
 
-(setq gnus-subscribe-newsgroup-method
-      'gnus-subscribe-killed            ; Don't subscribe to newsgroups automagically.
+(setq 
       gnus-use-generic-form nil         ;
       gnus-auto-select-first nil        ; Just show the summary, don't fetch the first article.
       gnus-auto-select-next nil         ; Same thing with next article.
@@ -77,7 +64,7 @@
       gnus-save-killed-list nil         ; Since we don't bother with new groups, don't remember what groups are killed.
       gnus-read-active-file 'some)      ; Don't read the entire friggin' active file when we connect.
 
-(setq gnus-use-adaptive-scoring t)
+
 
 
 ;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;;
@@ -94,28 +81,17 @@
 ;;; Wash HTML encoded articles with the built in function.
 ;;(setq mm-text-html-renderer 'html2text)
 
-;;; Customizing threading in Gnus
-
-;;; Yes, please do threading. But don't bore us; hide things like
-;;; subject and killed, but not living subtrees.
-(setq gnus-show-threads t
-      gnus-thread-hide-subject t
-      gnus-thread-hide-subtree nil
+(setq 
+      
+      
       gnus-thread-hide-killed t
       gnus-thread-ignore-subject nil
       gnus-use-cross-reference nil
       gnus-nov-is-evil nil)
 
 
-;;; Gather subjects by fuzzy string matching in the same thread if we
-;;; don't have any References.
-(setq gnus-summary-gather-subject-limit 'fuzzy)
 
-;;; Gather threads with use of References header.
-(setq gnus-summary-thread-gathering-function
-      'gnus-gather-threads-by-references)
 
-(setq gnus-summary-make-false-root 'adopt)
 
 ;;; My own key bindings.
 ;;;
@@ -136,25 +112,83 @@
 
 ;;; I want to see To: headers instead of From: headers in my outgoing
 ;;; archive groups.
-(setq gnus-extra-headers '(To Newsgroups))
-(setq nnmail-extra-headers gnus-extra-headers)
+;; (setq gnus-extra-headers '(To Newsgroups))
+;; (setq nnmail-extra-headers gnus-extra-headers)
 
 ;; (add-hook 'gnus-part-display-hook 'gnus-article-date-user)
-(setq gnus-treat-buttonize t)
 
 ;; http://www.emacswiki.org/emacs/GroupParameters
 ;;(setq gnus-parameter-to-list-alist '(("stack.exchange" . "do-not-reply@stackexchange.com")))
 ;;(setq gnus-total-expirable-newsgroups (regexp-opt '("stack.exchange")))
 
 (setq
+
+ ;;
+ gnus-show-threads t
+
+ ;;
+ gnus-thread-hide-subject t
+
+ ;; 
+ gnus-thread-hide-subtree nil
+ 
+ ;;
+ gnus-summary-make-false-root 'adopt
+
+ ;; Gather threads with use of References header.
+ gnus-summary-thread-gathering-function 'gnus-gather-threads-by-references
+
+ ;; Gather subjects by fuzzy string matching in the same thread if we don't have any References.
+ gnus-summary-gather-subject-limit 'fuzzy
+
+ ;;
+ gnus-treat-buttonize t
+
+ ;;
+ gnus-use-adaptive-scoring t
+
+ ;; Don't subscribe to newsgroups automagically.
+ gnus-subscribe-newsgroup-method 'gnus-subscribe-killed
+
+ ;; send mail function
+ ;; send-mail-function 'sendmail-send-it
+ send-mail-function 'smtpmail-send-it
+ ;; message-send-mail-function 'sendmail-send-it
+ message-send-mail-function 'smtpmail-send-it
+
+ ;; use gnus to send mail
+ mail-user-agent 'gnus-user-agent
+
+ ;; turn of nntp server
+ gnus-nntp-server nil
+
+ ;; Fetch no older articles, but thread everything we got.
+ ;; (setq gnus-fetch-old-headers 'nil)
+ gnus-fetch-old-headers 'some
+
  ;; prevent save dribble file message
  gnus-use-dribble-file nil
 
  ;; Cache ticked and dormant articles
  gnus-use-cache t
 
- ;; Default thread sorting
- gnus-thread-sort-functions '(gnus-thread-sort-by-total-score)
+;; Default thread sorting
+;; par date (plus recent en premier) puis par sujets
+;;;(setq gnus-thread-sort-functions
+;;;      '(
+;;;	gnus-thread-sort-by-total-score
+;;;        gnus-thread-sort-by-subject
+;;;        (not gnus-thread-sort-by-date)
+;;;        ))
+
+;;; gnus-thread-sort-functions '(gnus-thread-sort-by-total-score
+;;;			      gnus-thread-sort-by-date
+;;;			      )
+ gnus-thread-sort-functions '(gnus-thread-sort-by-total-score
+			      gnus-thread-sort-by-number
+			      gnus-thread-sort-by-most-recent-date)
+
+ gnus-sort-gathered-threads-function 'gnus-thread-sort-by-date
 
  ;; Suppress duplicate article
  gnus-suppress-duplicates t
@@ -180,4 +214,17 @@
 
  gnus-group-mode-hook '(gnus-topic-mode gnus-agent-mode)
 
+ ;; fetch all messages and never expire them
+ gnus-agent-cache t
+
+ ;; expire certain directories
+ gnus-auto-expirable-newsgroups "Trash"
+
+ ;; Buttonize the different parts, please
+ gnus-buttonized-mime-types '("multipart/encrypted" "multipart/signed")
+
+ ;; But keep buttons for multiple parts
+ gnus-inhibit-mime-unbuttonizing t
+
  )
+
