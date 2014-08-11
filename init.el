35ccd52f9e8684165f4dd66a4771d9209346371c
Paulo Jorge Tomé <paulo.jorge.tome@gmail.com>
2014-08-11 01:18:46 +0100

    Updated file 'init.el'.

--- init.el
+++ init.el
@@ -1584,10 +1584,10 @@ Late deadlines first, then scheduled, then non-late deadlines"
 
 ;; The following custom-set-faces create the highlights
 (custom-set-faces
-       ;; custom-set-faces was added by Custom.
-       ;; If you edit it by hand, you could mess it up, so be careful.
-       ;; Your init file should contain only one such instance.
-       ;; If there is more than one, they won't work right.
+  ;; custom-set-faces was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
  '(org-mode-line-clock ((t (:background "grey75" :foreground "red" :box (:line-width -1 :style released-button)))) t))
 
 
@@ -1955,3 +1955,9 @@ by using nxml's indentation rules."
 (setq auto-mode-alist
       (cons '("\\.po\\'\\|\\.po\\." . po-mode) auto-mode-alist))
 (autoload 'po-mode "po-mode" "Major mode for translators to edit PO files" t)
+(custom-set-variables
+  ;; custom-set-variables was added by Custom.
+  ;; If you edit it by hand, you could mess it up, so be careful.
+  ;; Your init file should contain only one such instance.
+  ;; If there is more than one, they won't work right.
+ '(fill-column 80))
