b68b5559644845a26f9085124c73dffa367b7d64
Paulo Jorge Tomé <paulo.jorge.tome@gmail.com>
2014-11-10 22:21:47 +0000

    modified:   init.el

--- init.el
+++ init.el
@@ -2044,7 +2044,6 @@ Finaly, blinks at the end of the marked region."
 
 (add-hook 'text-mode-hook 'turn-on-auto-fill)
 
-
 (add-hook 'org-mode-hook
           (lambda ()
             (set-fill-column 80)))
@@ -2113,8 +2112,24 @@ Finaly, blinks at the end of the marked region."
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(TeX-output-view-style (quote (("^pdf$" "." "iceweasel %o %(outpage)") ("^dvi$" ("^landscape$" "^pstricks$\\|^pst-\\|^psfrag$") "%(o?)dvips -t landscape %d -o && gv %f") ("^dvi$" "^pstricks$\\|^pst-\\|^psfrag$" "%(o?)dvips %d -o && gv %f") ("^dvi$" ("^\\(?:a4\\(?:dutch\\|paper\\|wide\\)\\|sem-a4\\)$" "^landscape$") "%(o?)xdvi %dS -paper a4r -s 0 %d") ("^dvi$" "^\\(?:a4\\(?:dutch\\|paper\\|wide\\)\\|sem-a4\\)$" "%(o?)xdvi %dS -paper a4 %d") ("^dvi$" ("^\\(?:a5\\(?:comb\\|paper\\)\\)$" "^landscape$") "%(o?)xdvi %dS -paper a5r -s 0 %d") ("^dvi$" "^\\(?:a5\\(?:comb\\|paper\\)\\)$" "%(o?)xdvi %dS -paper a5 %d") ("^dvi$" "^b5paper$" "%(o?)xdvi %dS -paper b5 %d") ("^dvi$" "^letterpaper$" "%(o?)xdvi %dS -paper us %d") ("^dvi$" "^legalpaper$" "%(o?)xdvi %dS -paper legal %d") ("^dvi$" "^executivepaper$" "%(o?)xdvi %dS -paper 7.25x10.5in %d") ("^dvi$" "." "%(o?)xdvi %dS %d") ("^pdf$" "." "xpdf -remote %s -raise %o %(outpage)") ("^html?$" nil "iceweasel %o"))))
+ '(TeX-output-view-style (quote (("^pdf$" "." "iceweasel %o %(outpage)")
+				 ("^dvi$" ("^landscape$" "^pstricks$\\|^pst-\\|^psfrag$") "%(o?)dvips -t landscape %d -o && gv %f")
+				 ("^dvi$" "^pstricks$\\|^pst-\\|^psfrag$" "%(o?)dvips %d -o && gv %f")
+				 ("^dvi$" ("^\\(?:a4\\(?:dutch\\|paper\\|wide\\)\\|sem-a4\\)$" "^landscape$") "%(o?)xdvi %dS -paper a4r -s 0 %d")
+				 ("^dvi$" "^\\(?:a4\\(?:dutch\\|paper\\|wide\\)\\|sem-a4\\)$" "%(o?)xdvi %dS -paper a4 %d")
+				 ("^dvi$" ("^\\(?:a5\\(?:comb\\|paper\\)\\)$" "^landscape$") "%(o?)xdvi %dS -paper a5r -s 0 %d")
+				 ("^dvi$" "^\\(?:a5\\(?:comb\\|paper\\)\\)$" "%(o?)xdvi %dS -paper a5 %d")
+				 ("^dvi$" "^b5paper$" "%(o?)xdvi %dS -paper b5 %d")
+				 ("^dvi$" "^letterpaper$" "%(o?)xdvi %dS -paper us %d")
+				 ("^dvi$" "^legalpaper$" "%(o?)xdvi %dS -paper legal %d")
+				 ("^dvi$" "^executivepaper$" "%(o?)xdvi %dS -paper 7.25x10.5in %d")
+				 ("^dvi$" "." "%(o?)xdvi %dS %d")
+				 ("^pdf$" "." "xpdf -remote %s -raise %o %(outpage)")
+				 ("^html?$" nil "iceweasel %o"))))
  '(TeX-source-correlate-method (quote synctex))
  '(TeX-source-correlate-mode t)
  '(TeX-source-correlate-start-server t)
- '(TeX-view-program-list (quote (("Iceweasel" "iceweasel --page-index=%(outpage)") ("DVI Viewer" "iceweasel %o") ("PDF Viewer" "iceweasel %o") ("Google Chrome" "google-chrome %o")))))
+ '(TeX-view-program-list (quote (("Iceweasel" "iceweasel --page-index=%(outpage)")
+				 ("DVI Viewer" "iceweasel %o")
+				 ("PDF Viewer" "iceweasel %o")
+				 ("Google Chrome" "google-chrome %o")))))
