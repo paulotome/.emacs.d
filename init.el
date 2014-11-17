e89c555b22769ca44ad02e48c9c2dd1abbec6660
Paulo Tome <paulo.tome@siscog.pt>
2014-11-17 10:27:47 +0000

    Grep mode hook command history

--- init.el
+++ init.el
@@ -2134,3 +2134,41 @@ Finaly, blinks at the end of the marked region."
 				 ("DVI Viewer" "iceweasel %o")
 				 ("PDF Viewer" "iceweasel %o")
 				 ("Google Chrome" "google-chrome %o")))))
+
+;; Persistent command history in grep mode
+
+;; http://oleksandrmanzyuk.wordpress.com/2011/10/23/a-persistent-command-history-in-emacs/
+
+(defun comint-write-history-on-exit (process event)
+  (comint-write-input-ring)
+  (let ((buf (process-buffer process)))
+    (when (buffer-live-p buf)
+      (with-current-buffer buf
+        (insert (format "\nProcess %s %s" process event))))))
+
+
+(defun turn-on-comint-history ()
+  (let ((process (get-buffer-process (current-buffer))))
+    (when process
+      (setq comint-input-ring-file-name
+            (format "~/.emacs.d/inferior-%s-history"
+                    (process-name process)))
+      (comint-read-input-ring)
+      (set-process-sentinel process
+                            #'comint-write-history-on-exit))))
+
+
+(defun mapc-buffers (fn)
+  (mapc (lambda (buffer)
+          (with-current-buffer buffer
+            (funcall fn)))
+        (buffer-list)))
+
+(defun comint-write-input-ring-all-buffers ()
+  (mapc-buffers 'comint-write-input-ring))
+
+
+(add-hook 'grep-mode-hook 'turn-on-comint-history)
+
+
+(add-hook 'kill-emacs-hook 'comint-write-input-ring-all-buffers)
